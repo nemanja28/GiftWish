501822bff18e1857c47b87c603ddc1a46b92cd7d
Nemanja Stosic <nemanja.stosic@rbt.rs>
2019-03-20 16:26:47 +0100

    Fix bug of infinity splash screen

--- app/src/main/java/rs/rbt/giftwishlist/splash/SplashViewModel.kt
+++ app/src/main/java/rs/rbt/giftwishlist/splash/SplashViewModel.kt
@@ -4,18 +4,25 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.broadcast
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.warn
 import rs.rbt.giftwishlist.SingleLiveEvent
 import rs.rbt.giftwishlist.data.authorization.AuthorizationRepository
 import kotlin.coroutines.CoroutineContext
 
-class SplashViewModel(application: Application, private val authorizationRepository: AuthorizationRepository) : AndroidViewModel(application) {
+class SplashViewModel(application: Application, private val authorizationRepository: AuthorizationRepository) : AndroidViewModel(application), AnkoLogger {
 
     val onLoginSuccessEvent = SingleLiveEvent<Void>()
     val onLoginFailedEvent = SingleLiveEvent<String>()
 
+    private var uiScope: CoroutineScope? = null
+
     fun viewDidLoad() {
-        viewModelScope.launch {
-            delay(3000)
+        val viewModelJob = SupervisorJob()
+        uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+        uiScope?.launch {
+            delay(SIMULATED_LOADING_DELAY_IN_MS)
             loginUserAutomaticallyIfPossible()
         }
     }
@@ -28,9 +35,14 @@ class SplashViewModel(application: Application, private val authorizationReposit
         }
     }
 
-
-    companion object {
-        const val NO_REASON_TEXT = ""
+    @ExperimentalCoroutinesApi
+    override fun onCleared() {
+        super.onCleared()
+        uiScope?.cancel()
     }
 
+    companion object {
+        const val SIMULATED_LOADING_DELAY_IN_MS = 3000L
+        const val NO_REASON_TEXT = ""
+    }
 }
